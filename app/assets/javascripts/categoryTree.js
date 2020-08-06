11d7afba3a0a8b5191888b62a1f63cdc7bfdd908
itoutakeshi <take339@hotmail.com>
2020-08-06 12:43:42 +0900

    2020 0806 new

--- app/assets/javascripts/categoryTree.js
+++ app/assets/javascripts/categoryTree.js
@@ -1,12 +1,8 @@
 
 $(function(){
   $('li.lists__left__item--category').hover(function(){
-    //$("ul:not(:animated)", this).slideDown();
     $(".tree__display").css('display','block');
-    
-      console.log("hover")
   }, function(){
       $(".tree__display").css('display','none');
-      console.log("not hover")
   });
 });
