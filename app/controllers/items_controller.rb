8f57aaefdb950c867e3f634502f863bcce1f0bd7
KeigoOhashi <knock-out-o_o-84@ezweb.ne.jp>
2020-08-15 16:15:56 +0900

    0815_deploy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    if user_signed_in?
+      @product = Product.new
+      @product.images.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.images.present?
+      @product.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 end
