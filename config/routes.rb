6ac4265bd055761d77c6ef622ed29cc0fe3db9bc
itoutakeshi <odoruryourinin@gmail.com>
2020-08-18 02:06:59 +0900

    0818 pullup 商品出品ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :products, only: [:index, :show] do
+  resources :products, only: [:index,:new, :show] do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
