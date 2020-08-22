fd3c1732ff6aa5a78da4471c869f4025381f27b8
KeigoOhashi <knock-out-o_o-84@ezweb.ne.jp>
2020-08-22 14:15:28 +0900

    商品出品機能（アソシエーション・バリデーション）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_083642) do
+ActiveRecord::Schema.define(version: 2020_08_22_042529) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image_url", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -47,5 +55,6 @@ ActiveRecord::Schema.define(version: 2020_08_13_083642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "images", "products"
   add_foreign_key "products", "users"
 end
