f12adf436e84b85544077184321774bd18255182
KeigoOhashi <knock-out-o_o-84@ezweb.ne.jp>
2020-08-25 18:08:48 +0900

    S3 アップロード

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,31 @@
 class ProductsController < ApplicationController
-def new
+  def index
+  end
 
-end
-def show
-  
-end
+  def new
+    if user_signed_in?
+      @product = Product.new
+      @product.images.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.images.present? && @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  def show
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name,:price,:description,:brand,:status,:postage,:shipping_area,:shipping_days,images_attributes: [:product_image,:_destroy,:id]).merge(user_id: current_user.id)
+    end
+    
+  end
 end
