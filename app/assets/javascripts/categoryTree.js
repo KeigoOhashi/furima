27b8399651cd5f8009e6703ca1dbaa4fb21db716
itoutakeshi <odoruryourinin@gmail.com>
2020-09-02 04:21:23 +0900

    0902 category

--- app/assets/javascripts/categoryTree.js
+++ app/assets/javascripts/categoryTree.js
@@ -1,7 +1,7 @@
-$(function(){
-  $('li.lists__left__item--category').hover(function(){
-    $(".tree__display").css('display','block');
-  }, function(){
-      $(".tree__display").css('display','none');
-  });
-});
+//$(function(){
+//  $('li.lists__left__item--category').hover(function(){
+//    $(".tree__display").css('display','block');
+//  }, function(){
+//      $(".tree__display").css('display','none');
+//  });
+//});
